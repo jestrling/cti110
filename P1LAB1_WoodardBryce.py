0b3b85d3aaa6788f86f1d18b4cbe8c6ad413224e
jestrling <woodardb7942@student.faytechcc.edu>
2025-04-04 23:24:58 -0400

    Add files via upload

---
+++ P1LAB1_WoodardBryce.py
@@ -0,0 +1,9 @@
+# Your Name: Bryce Woodard
+# Date: April 4, 2025
+# Assignment Name: P1LAB1
+# This program asks the user for their first and last name, then displays a personalized greeting message with their full name and a course welcome.
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+
+print(f"Hello {first_name} {last_name}, Welcome to CTI-110!")
